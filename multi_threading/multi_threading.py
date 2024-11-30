f8079b00f8ae6eaaa0175b5b2e50663334c4e99a
sungeun <58740211+jake-hong@users.noreply.github.com>
2024-11-30 23:06:55 +0900

    python threading에 대한 이해.

--- multi_threading/multi_threading.py
+++ multi_threading/multi_threading.py
@@ -1,6 +1,6 @@
 """
 Multithreading 
-스레드와 프로세스의 차이 
+스레드와 프로세스의 차이, GIL
 
 1) 프로세스
 - 운영체제에서 할당받는 자원의 단위(실행중인 프로그램)
@@ -30,4 +30,46 @@ Multithreading
 - 한 개의 프로세스 문제 발생은 확산이 없다. (process kill)
 - 캐시 체인지를 활용함(cost 비용이 매우 높음- overhead)
 - 복잡한 통신 방식
+
+5) GIL(Global Interpreter Lock)
+- Cpython이 python(bytecode) 실행 시 여러 thread를 사용할 경우 하나의 단일 스레드만 python object에 접근하도록 or 사용하도록 제한을 건 것. 
+- Cpython 메모리 관리가 취약해서..(thread-safe를 위해 GIL을 검)
+- 단일 스레드도 충분히 빠르다. 
+- 멀티 프로세스도 사용 가능(Numpy/scipy) 
+- 병렬 처리는 multiprocessing을 쓰면 됨. asyncio를 써도 됨.
+- thread 동시성을 완벽하게 처리하기 위한 Jython, IronPython, stackelss python 등이 있음.
 """
+
+"""
+Python Thread
+
+thread는 main area와 상관 없이 sub thread의 일이 끝나야 끝남.
+"""
+import logging
+import threading
+import time
+
+
+def thread_func(name):
+    logging.info(f"Sub-Thread {name}: start")
+    time.sleep(2)
+    logging.info(f"Sub-Thread {name}: finish")
+    
+# main area
+if __name__ == "__main__":
+    # logging format
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("Main-thread: before create thread")
+    
+    thread = threading.Thread(target=thread_func, args= ("First",))
+    logging.info("Main-Thread: before run thread")
+    
+    # start sub-thread 
+    thread.start()
+    
+    #join()을 추가하면 sub thread가 먼저 끝날때까지 기다림
+    thread.join()
+    
+    logging.info("Main-Thread: wait for the thread to finish")
+    logging.info("Main-Thread: all done")
